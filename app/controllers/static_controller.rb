17dcd32388b5246c349c3b0d9dbc99b78b2e4a79
Kim Pham <pham.kim.a@gmail.com>
2018-09-22 17:05:26 -0500

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,9 @@
+# static controller that inherits from the application controller and 
+# contains the hello_world action
+
+class StaticController < ApplicationController
+
+    def hello_world
+
+    end
+end
